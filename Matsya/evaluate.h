93c207c293034712058ce879730b9e0df5ae993c
Nilesh <nileshsatishkulkarni@gmail.com>
2013-09-23 22:46:22 +0530

    minor change

--- Matsya/evaluate.h
+++ Matsya/evaluate.h
@@ -11,14 +11,13 @@ class Evaluate{
 		tree_t* _root;
 		map<std::string, int> _environment;
 
-
+		int _executeAssign(tree_t* t);
+		int _executeStmtseq(tree_t*);
+		int _executeStatement(tree_t*);
 		int _executePrint(tree_t* t);
 		int _executeIfOnly(tree_t* t);
 		int _executeIf(tree_t* t);
 		int _executeWhile(tree_t* t);
-		int _executeAssign(tree_t* t);
-		int _executeStmtseq(tree_t*);
-		int _executeStatement(tree_t*);
 		int _executeExpression(tree_t*);
 		
 	public:
